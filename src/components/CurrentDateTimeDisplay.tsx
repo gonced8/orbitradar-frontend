917202334093a3b082ac7918537ceb9e08398861
Gonçalo Raposo <gonced8@gmail.com>
2026-06-28 23:34:19 +0100

    Add GitHub workflows and code owners

--- src/components/CurrentDateTimeDisplay.tsx
+++ src/components/CurrentDateTimeDisplay.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const CurrentDateTimeDisplay = () => {
   const [dateTime, setDateTime] = useState(new Date());
@@ -13,14 +13,14 @@ const CurrentDateTimeDisplay = () => {
     };
   }, []);
 
-  const formatter = new Intl.DateTimeFormat('en-CA', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    hour12: false
+  const formatter = new Intl.DateTimeFormat("en-CA", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
   });
 
   return (
